cf96698584b85d456b82599784aa5f8a40b215e2
Carl Schwan <carl@carlschwan.eu>
2019-07-07 10:59:00 +0200

    Remove tabs

--- Aether.php
+++ Aether.php
@@ -12,12 +12,12 @@
 <?php
 
 if ( function_exists( 'wfLoadSkin' ) ) {
-	wfLoadSkin( 'Aether' );
-	/* wfWarn(
-		'Deprecated PHP entry point used for Vector skin. Please use wfLoadSkin instead, ' .
-		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
-	return true;
+    wfLoadSkin( 'Aether' );
+    /* wfWarn(
+        'Deprecated PHP entry point used for Vector skin. Please use wfLoadSkin instead, ' .
+        'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+    ); */
+    return true;
 } else {
-	die( 'This version of the Neverland skin requires MediaWiki 1.25+' );
+    die( 'This version of the Neverland skin requires MediaWiki 1.25+' );
 }
