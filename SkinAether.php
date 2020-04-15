6411b38254c4bc8e026a92e4f541aa8831d9b5b8
Mediawiki Administrator <wikiadmin@edulis.kde.org>
2020-04-15 20:07:48 +0000

    Revert "small patch"
    This reverts commit 917cc4593899c39f3df3575081ed2f4bd37d6726.

--- SkinAether.php
+++ SkinAether.php
@@ -1,16 +1,13 @@
 <?php
 /**
- * Aether
+ * Neverland - Modern version of MonoBook with fresh look and many usability
+ * improvements.
+ *
+ * @todo document
  * @file
  * @ingroup Skins
  */
 
-require_once __DIR__.'/vendor/autoload.php';
-require_once 'JsonManifestNetworkStrategy.php';
-
-use Symfony\Component\Asset\UrlPackage;
-use Symfony\Component\Cache\Adapter\FilesystemAdapter;
-use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * SkinTemplate class for Neverland skin
@@ -40,7 +37,6 @@ class SkinAether extends SkinTemplate {
         // this is better than including this in a CSS fille since it doesn't
         // wait for the CSS file to load before fetching the HTC file.
         $min = $this->getRequest()->getFuzzyBool( 'debug' ) ? '' : '.min';
-        $out->addHeadItem('abc', '<script type="text/javascript" src="https://cdn.kde.org/aether/js/jquery.min.js"></script> <script type="text/javascript" src="/skins/Aether/resources/jquery.ba-throttle-debounce.min.js"></script> <script type="text/javascript" src="https://cdn.kde.org/aether/js/popper.min.js"></script> <script type="text/javascript" src="https://cdn.kde.org/aether/js/bootstrap.min.js"></script> <script type="text/javascript" src="/skins/Aether/resources/main.js"></script>');
         $out->addModules( array('skins.aether') );
         $out->addMeta( 'viewport', 'width=device-width, initial-scale=1, shrink-to-fit=no' );
     }
@@ -49,24 +45,9 @@ class SkinAether extends SkinTemplate {
      * Load skin and user CSS files in the correct order
      * @param $out OutputPage object
      */
-    function setupSkinUserCss(OutputPage $out) {
-        $cache = new FilesystemAdapter();
-        // $cache->delete('bootstrapcss');
-        $bootstrapcss = $cache->get('bootstrapcss', function (ItemInterface $item) {
-            $item->expiresAfter(600);
-            $urlPackage = new UrlPackage(
-                'https://cdn.kde.org/',
-                new JsonManifestNetworkVersionStrategy('https://cdn.kde.org/aether-devel/version/manifest.json')
-            );
-            $boostrapcss = $urlPackage->getUrl('aether-devel/version/bootstrap.css');
-
-            return $boostrapcss;
-        });
-
-        $out->addStyle($bootstrapcss, 'all');
-
-        //$out->addStyle( $urlPackage->getUrl('aether-devel/version/bootstrap.css'), 'all' );
-        //$out->addStyle( $urlPackage->getUrl('aether-devel/version/aether-mediawiki.css'), 'all' );
+    function setupSkinUserCss( OutputPage $out ){
+        $out->addStyle( 'https://cdn.kde.org/aether/css/bootstrap.min.css', 'all' );
+        $out->addStyle(  $this->stylename . '/resources/main.css', 'all' );
     }
 
     /**
